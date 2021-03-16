3a5476b0eae57891b34428ff4c882e03ea3e01b2
chiukovo <chiuko@celloes.com>
2021-03-16 17:54:48 +0800

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -171,28 +171,6 @@
         </div>
     </div>
     <!-- //banner -->
-    <!-- modal -->
-    <div class="modal about-modal fade" id="myModal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title">Free <span>Online</span></h4>
-                </div>
-                <div class="modal-body">
-                    <div class="agileits-w3layouts-info">
-                        <img src="images/2.jpg" alt="" />
-                        <p>Duis venenatis, turpis eu bibendum porttitor, sapien quam ultricies tellus, ac rhoncus risus odio eget
-                            nunc. Pellentesque ac fermentum diam. Integer eu facilisis nunc, a iaculis felis. Pellentesque
-                            pellentesque tempor enim, in dapibus turpis porttitor quis. Suspendisse ultrices hendrerit massa. Nam id
-                            metus id tellus ultrices ullamcorper. Cras tempor massa luctus, varius lacus sit amet, blandit lorem. Duis
-                            auctor in tortor sed tristique. Proin sed finibus sem.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- //modal -->
     <!-- about -->
     <div class="about" id="about">
         <div class="container">
